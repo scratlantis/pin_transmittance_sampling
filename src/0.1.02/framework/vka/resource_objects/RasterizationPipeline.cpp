a9b8903682769888898885c9c835e7df34ffc76b
Sidney <sidney.hansen@student.kit.edu>
2024-06-04 19:45:09 +0200

    added pipeline definition members

--- src/0.1.02/framework/vka/resource_objects/RasterizationPipeline.cpp
+++ src/0.1.02/framework/vka/resource_objects/RasterizationPipeline.cpp
@@ -51,6 +51,25 @@ VkPipeline RasterizationPipeline::getHandle() const
 
 RasterizationPipeline::RasterizationPipeline(RestarizationPipelineDefinition const &definition)
 {
+	VkGraphicsPipelineCreateInfo ci{VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO};
+	ci.flags;
+	ci.stageCount;
+	ci.pStages;
+	ci.pVertexInputState;
+	ci.pInputAssemblyState;
+	ci.pTessellationState;
+	ci.pViewportState;
+	ci.pRasterizationState;
+	ci.pMultisampleState;
+	ci.pDepthStencilState;
+	ci.pColorBlendState;
+	ci.pDynamicState;
+	ci.layout;
+	ci.renderPass;
+	ci.subpass;
+	ci.basePipelineHandle;
+	ci.basePipelineIndex;
+	VK_CHECK(vkCreateGraphicsPipelines(gState.device.logical, VK_NULL_HANDLE, 1, &ci, nullptr, &handle));
 }
 
 }        // namespace vka
--- src/0.1.02/framework/vka/resource_objects/RasterizationPipeline.h
+++ src/0.1.02/framework/vka/resource_objects/RasterizationPipeline.h
@@ -8,10 +8,36 @@
 
 namespace vka
 {
+struct GlobalColorBlendState
+{
+	VkPipelineColorBlendStateCreateFlags flags;
+	VkBool32                             logicOpEnable;
+	VkLogicOp                            logicOp;
+	float                                blendConstants[4];
+};
 class RestarizationPipelineDefinition : public ResourceIdentifier
 {
   public:
-	
+	VkPipelineCreateFlags                            flags;
+	std::vector<ShaderDefinition>                    shaderDefinitions;
+	std::vector<VkVertexInputBindingDescription>     vertexBindingDescriptions;
+	std::vector<VkVertexInputAttributeDescription>   vertexAttributeDescriptions;
+	VkPipelineInputAssemblyStateCreateInfo           inputAssemblyState;
+	VkPipelineTessellationStateCreateInfo            tessellationState;
+	std::vector<VkViewport>                          viewport;
+	std::vector<VkRect2D>                            scissor;
+	VkPipelineRasterizationStateCreateInfo           rasterizationState;
+	std::vector<VkSampleMask>                        sampleMasks;
+	VkPipelineMultisampleStateCreateInfo             multisampleState;
+	VkPipelineDepthStencilStateCreateInfo            depthStencilState;
+	GlobalColorBlendState                            globalColorBlendState;
+	std::vector<VkPipelineColorBlendAttachmentState> colorBlendAttachmentStates;
+	std::vector<VkDynamicState>                      dynamicStates;
+	PipelineLayoutDefinition                         pipelineLayoutDefinition;
+	RenderPassDefinition                             renderPassDefinition;
+	uint32_t                                         subpass;
+
+
 
 	hash_t hash() const;
 	bool   _equals(ResourceIdentifier const &other) const override;
