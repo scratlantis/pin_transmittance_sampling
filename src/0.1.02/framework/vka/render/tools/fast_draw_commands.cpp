19ffe2d8929d48984428a6bcbc06489d6ab23a7e
Sidney Hansen <sidney.hansen@gmx.de>
2024-07-01 15:25:07 +0200

    fixed histogram average

--- src/0.1.02/framework/vka/render/tools/fast_draw_commands.cpp
+++ src/0.1.02/framework/vka/render/tools/fast_draw_commands.cpp
@@ -73,6 +73,13 @@ void FastDrawState::computeHistogram(VkaCommandBuffer cmdBuf, VkaImage src, Samp
 	dst->changeMemoryType(VMA_MEMORY_USAGE_GPU_ONLY);
 	dst->recreate();
 	vkaCmdFillBuffer(cmdBuf, dst, 0, dst->getSize(), 0);
+
+	average->changeSize(sizeof(float));
+	average->addUsage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT);
+	average->changeMemoryType(VMA_MEMORY_USAGE_GPU_ONLY);
+	average->recreate();
+	vkaCmdFillBuffer(cmdBuf, average, 0.f, average->getSize(), 0);
+
 	VkaBuffer ubo = vkaCreateBuffer(gState.frame->stack, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT);
 	vkaWriteStaging(ubo, static_cast<const void *>(&area), sizeof(VkRect2D_OP));
 	vkaCmdUpload(cmdBuf, ubo);
@@ -81,14 +88,11 @@ void FastDrawState::computeHistogram(VkaCommandBuffer cmdBuf, VkaImage src, Samp
 	addDescriptor(computeCmd, dst, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
 	addDescriptor(computeCmd, pSamplerDef, VK_DESCRIPTOR_TYPE_SAMPLER);
 	addDescriptor(computeCmd, src, VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE);
+	addDescriptor(computeCmd, average, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
 	vkaCmdCompute(cmdBuf, computeCmd);
 
 	vkaCmdBarrier(cmdBuf, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_SHADER_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT);
 
-	average->changeSize(sizeof(uint32_t));
-	average->addUsage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
-	average->changeMemoryType(VMA_MEMORY_USAGE_GPU_ONLY);
-	average->recreate();
 	setDefaults(computeCmd, 1U, gVkaShaderPath + "histogram_sum.comp");
 	addDescriptor(computeCmd, ubo, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
 	addDescriptor(computeCmd, dst, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
